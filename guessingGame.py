1aedaf701f8e977c2d7dea3958f2bd242e019e96
AaditMohatta <62171882+AaditMohatta@users.noreply.github.com>
2021-12-26 17:17:41 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,15 @@
+import random
+value = random.choice(0,9)
+print(value)
+def guesingGame():
+    number = input("Tell me your number from 1 to 9:- ")
+    print(value)
+    for number == value:
+        print("Congratulations you Win!!")
+     else:
+         print("You Lose!!The number is "number)
+
+
+    
+
+    
